b96484183d1881de6a0d39e1bec0dbcbd28ff185
Pankaj Poul <pankajpoul@logivention.in>
2026-01-17 01:01:04 +0530

    Added HR Version V9

--- src/components/hr/SubmitTimesheetModal.jsx
+++ src/components/hr/SubmitTimesheetModal.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useMsal } from "@azure/msal-react";
 import { getAccessToken } from "../../auth/authService";
 import {
